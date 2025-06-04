b3d40cd9caacec3d0d30fea5ea0414fb06866a5d
Ramu Narasinga <ramu.narasinga@gmail.com>
2025-06-04 22:46:52 +0530

    feat: headers configured with cookies to fetch the list of users

--- src/get-users.ts
+++ src/get-users.ts
@@ -1,7 +1,12 @@
+import { headers } from "./utils/headers";
+
 export async function getUsers() {
     const url = "https://challenge.sunvoy.com/api/users";
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers
+        });
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
